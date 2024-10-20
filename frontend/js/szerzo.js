a263159b00f0e866e169bca7d20f64f8a47b0dfb
UtasiB <utasibalazs@turr.hu>
2024-10-20 18:10:49 +0200

    Szerzos dolog kész

--- frontend/js/szerzo.js
+++ frontend/js/szerzo.js
@@ -1,84 +1,64 @@
 
 var xhrSzerzo = new XMLHttpRequest();
-function onSzerzoLoad(){
+function onSzerzoLoad() {
     xhrSzerzo.open('GET', 'http://localhost:5000/authors', true);
     xhrSzerzo.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     xhrSzerzo.send();
-    xhrSzerzo.onreadystatechange = function(){
-        if(xhrSzerzo.readyState == 4){
+    xhrSzerzo.onreadystatechange = function () {
+        if (xhrSzerzo.readyState == 4 && xhrSzerzo.status === 200) {
             var responseData = JSON.parse(xhrSzerzo.responseText);
             console.log(responseData);
-            
-            // Tábla feltöltés
-            let tbody = document.querySelector('#authorTbody');
-            tbody.innerHTML = '';
-            
-            
-      
-           // egyseg.innerHTML = '<option selected>Válaszd ki az egységet</option>'; 
 
-            responseData.forEach((item) =>{
+            let tbody = document.querySelector('#authorTbody');
+            tbody.innerHTML = ''; 
+
+            responseData.forEach((item) => {
                 let tr = document.createElement('tr');
-                let td0 = document.createElement('td')
+                let td0 = document.createElement('td');
                 let td1 = document.createElement('td');
                 let td2 = document.createElement('td');
-                
-               
 
                 td0.innerHTML = item.id;
-                td1.innerHTML = item.name;    
+                td1.innerHTML = item.name;
                 td2.innerHTML = moment(item.birthdate).format('YYYY-MM-DD');
-               
-               
-
-                
-                let deleteButton = document.createElement('button');
-                deleteButton.innerText = 'Törlés';
-                deleteButton.classList.add('btn', 'btn-danger');
-                deleteButton.onclick = (event) => {
-                    event.stopPropagation(); 
-                    deleteRow(item);
-                };
-
                 let updateButton = document.createElement('button');
                 updateButton.innerText = 'Frissítés';
                 updateButton.classList.add('btn', 'btn-secondary');
                 updateButton.onclick = (event) => {
                     event.stopPropagation();
-                    // Hívjuk meg a frissítést a kiválasztott szerző ID-jével
-                    updateSzerzo(item.id); // Pass the item ID to the updateSzerzo function
-                    render("updateAuthor");
+                    const szerzoId = item.id;
+                    window.location.href = `http://127.0.0.1:5500/frontend/views/updateAuthor.html?id=${szerzoId}`;
                 };
-                
-                
+
+                let deleteButton = document.createElement('button');
+                deleteButton.innerText = 'Törlés';
+                deleteButton.classList.add('btn', 'btn-danger');
+                deleteButton.onclick = (event) => {
+                    event.stopPropagation();
+                    deleteRow(item);
+                };
+
                 let tdUpdate = document.createElement('td');
                 tdUpdate.appendChild(updateButton);
-                tdUpdate.classList.add('text-end')
-                
-                
+                tdUpdate.classList.add('text-end');
+
                 let tdDelete = document.createElement('td');
                 tdDelete.appendChild(deleteButton);
-              
-                
 
                 tr.appendChild(td0);
-                tr.appendChild(td1);    
+                tr.appendChild(td1);
                 tr.appendChild(td2);
                 tr.appendChild(tdUpdate);
                 tr.appendChild(tdDelete);
                 tbody.appendChild(tr);
             });
-            
-          
         }
     }
 }
 onSzerzoLoad();
 
-
 function szerzoFeltoltes(event) {
-    event.preventDefault(); // Prevent the default form submission
-
+    event.preventDefault();
     var data = JSON.stringify({
         name: document.querySelector('#name').value,
         birthdate: document.querySelector('#birthdate').value        
@@ -91,7 +71,7 @@ function szerzoFeltoltes(event) {
     xhrSzerzo.onreadystatechange = function() {
         if (xhrSzerzo.readyState === 4) {
             alert(xhrSzerzo.responseText);
-            onSzerzoLoad(); // Reload author list after adding a new author
+            onSzerzoLoad(); 
         }
     };
 
@@ -99,53 +79,65 @@ function szerzoFeltoltes(event) {
 }
 
 function deleteRow(item) {
-    
-        var xhrSzerzoDelete = new XMLHttpRequest();
-        xhrSzerzoDelete.open('DELETE', `http://localhost:5000/authors/${item.id}`, true); 
-        xhrSzerzoDelete.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        
-        xhrSzerzoDelete.onreadystatechange = function() {
-            if (xhrSzerzoDelete.readyState === 4) {
-                if (xhrSzerzoDelete.status === 202) {
-                    alert('Sikeres törlés!'); 
-                   
-                    const rows = document.querySelectorAll('.table tbody tr');
-                    rows.forEach(row => {
-                        if (row.cells[1].innerText === item.termek) { 
-                            row.remove();
-                        }
-                    });
-                } else {
-                    alert(xhrSzerzoDelete.responseText);
-                }
-            }
-        };
-        
-        xhrSzerzoDelete.send();
+    var xhrSzerzoDelete = new XMLHttpRequest();
+    xhrSzerzoDelete.open('DELETE', `http://localhost:5000/authors/${item.id}`, true); 
+    xhrSzerzoDelete.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhrSzerzoDelete.send();
+
+    xhrSzerzoDelete.onreadystatechange = function() {
         onSzerzoLoad();
-    
-}
-
-function updateSzerzo(id) {
-    var data = JSON.stringify({
-        name: document.querySelector('#name').value,
-        birthdate: document.querySelector('#birthdate').value
-    });
-
-    var xhrSzerzoUpdate = new XMLHttpRequest();
-    xhrSzerzoUpdate.open('PATCH', `http://localhost:5000/authors/${id}`, true);
-    xhrSzerzoUpdate.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-    xhrSzerzoUpdate.onreadystatechange = function() {
-        if (xhrSzerzoUpdate.readyState === 4) {
-            if (xhrSzerzoUpdate.status === 200) {
-                alert('Frissítés sikeres!');
-                onSzerzoLoad(); // Reload author list after updating
+        if (xhrSzerzoDelete.readyState === 4) {
+            if (xhrSzerzoDelete.status === 202) {
             } else {
-                alert('Hiba történt: ' + xhrSzerzoUpdate.responseText);
+                alert(xhrSzerzoDelete.responseText);
             }
         }
     };
+}
 
-    xhrSzerzoUpdate.send(data);
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const szerzoId = getQueryParam('id');
+    console.log(szerzoId);
+    if (szerzoId) {
+        updateSzerzo(szerzoId); 
+    }
+});
+function updateSzerzo(id) {
+    console.log("updateSzerzo függvény fut ID-val: ", id);
+    fetch(`http://localhost:5000/authors/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#name').value = data.name;
+            document.querySelector('#birthdate').value = data.birthdate;
+        });
+    document.querySelector('form').onsubmit = function(event) {
+        event.preventDefault();
+        
+        var updatedData = JSON.stringify({
+            name: document.querySelector('#name').value,
+            birthdate: document.querySelector('#birthdate').value
+        });
+
+        var xhrSzerzoUpdate = new XMLHttpRequest();
+        xhrSzerzoUpdate.open('PATCH', `http://localhost:5000/authors/${id}`, true);
+        xhrSzerzoUpdate.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+
+        xhrSzerzoUpdate.onreadystatechange = function() {
+            if (xhrSzerzoUpdate.readyState === 4) {
+                if (xhrSzerzoUpdate.status === 200) {
+                    alert('Frissítés sikeres!');
+                    window.location.href = "../index.html";
+                } else {
+                    alert('Hiba történt: ' + xhrSzerzoUpdate.responseText);
+                }
+            }
+        };
+
+        xhrSzerzoUpdate.send(updatedData);
+    };
 }
